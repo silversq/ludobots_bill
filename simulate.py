d73a6f5f34e775cdfcc32919ddd90d9d3f3955f2
Bill Wang <67875325+silversq@users.noreply.github.com>
2023-01-05 15:32:49 -0600

    added length, width, height and floor

--- simulate.py
+++ simulate.py
@@ -1,7 +1,15 @@
 import pybullet as p
 import time
+import pybullet_data
+
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
 p.loadSDF("box.sdf")
+p.setGravity(0,0,-9.8)
+
+planeId = p.loadURDF("plane.urdf")
+
 
 for i in range(1000):
     p.stepSimulation()
